9b99a5eb3a5b77ce4d47f9579adccba478e6449f
Henok <henokcodes@gmail.com>
2021-11-20 20:28:09 -0800

    Adding Vue

---
+++ todolist/resources/js/vue/app.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="todoListContainer>
+        <div class="heading>
+            <h2 id="title"> Todo List</h2>
+            <add-item-form />
+        </div>
+    </div>
+</template>
+
+<script>
+import addItemForm from "./addItemForm"
+export default{
+    components: {
+        addItemForm
+    }
+}
+
+</script>
