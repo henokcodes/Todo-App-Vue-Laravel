76c0c503e6950d2a0e31ad9f14fc146cc807ade6
Henok <henokcodes@gmail.com>
2021-11-20 22:08:09 -0800

    Finish ups

--- resources/js/vue/addItemForm.vue
+++ resources/js/vue/addItemForm.vue
@@ -1,12 +1,62 @@
-<template>
-    <div>
-        List View 
+<template lang="">
+    <div class="addTodo">
+        <input type="text" v-model="item.name" />
+        <font-awesome-icon
+         icon="plus-square"
+         @click="addItem()"
+         :class="[ item.name ? 'active' : 'inactive','plus']" />
     </div>
 </template>
-
 <script>
-export default{
-    
+export default {
+      data: function()
+{
+    return {
+        item: {
+            name: ""
 }
-
+}},
+methods: {
+    addItem()
+{
+    if(this.item.name == ''){
+        return;
+        }
+    axios.post('api/item/store',{
+        item: this.item})
+        .then( response=>{
+            if( response.status ==201){
+                this.item.name = "";
+                this.$emit('reloadlist');
+                }
+                })
+                .catch( error => {
+                    console.log( error );
+                    })
+}
+}}
 </script>
+<style scoped>
+    .addTodo {
+    display:flex;  
+    justify-content: center;
+    align-items: center;
+}
+input {
+    background: #f7f7f7;
+    border: 0px;
+    outline: none;
+    padding: 5px;
+    margin-right: 10px;
+    width: 100%;
+    }
+.plus{
+    font-size: 20px;
+    }
+.active{
+    color: #00CE25;
+    }
+.inactive{
+    color: #999999;
+    }
+</style>
